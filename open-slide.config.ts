ed41b85c36cfbe870eba1d2aad1a5dc75a4594b7
nagisora <nagisora.icanspeng@gmail.com>
2026-05-21 20:40:58 +0900

    chore: init open-slide project

---
+++ open-slide.config.ts
@@ -0,0 +1,8 @@
+import type { OpenSlideConfig } from '@open-slide/core';
+import { ja } from '@open-slide/core/locale';
+
+const openSlideConfig: OpenSlideConfig = {
+  locale: ja,
+};
+
+export default openSlideConfig;
